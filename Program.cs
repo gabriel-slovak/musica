72907f7cb2fa2979b7fe9b28df8858ad98b942cd
Diego Neri <diego@diegoneri.com>
2021-03-09 22:23:20 -0300

    Ajusta endentação
    Ajusta a endentação do código

--- Program.cs
+++ Program.cs
@@ -1,53 +1,47 @@
-﻿using System;
+using System;
 
-namespace musica
-{
-    class Program
-    {
+namespace musica {
+    class Program {
         static void Main(string[] args)
-    
 
-        {  Console.WriteLine();
+        {
+            Console.WriteLine();
             Console.WriteLine("--------------------");
             Console.WriteLine("Atividade musica");
-             Console.WriteLine("--------------------");
-          Console.WriteLine();
- Console.ForegroundColor = ConsoleColor.Red; //deixar nome da musica em vermelho
-          Console.WriteLine("*Black sheep (From Scott Pilgrim vs. the World)*");
-          Console.ResetColor();
-           Console.WriteLine();
-        
-            Console.WriteLine("Hello again, friend of a friend I knew you when");
-             Console.WriteLine("Our common goal was waiting for the world to end");
-              Console.WriteLine("Now that the truth is just a rule that you can bend");
-               Console.WriteLine("You crack the whip, shape-shift and trick the past again");
-                Console.WriteLine();
+            Console.WriteLine("--------------------");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Red; //deixar nome da musica em vermelho
+            Console.WriteLine("*Black sheep (From Scott Pilgrim vs. the World)*");
+            Console.ResetColor();
+            Console.WriteLine();
 
- Console.ForegroundColor = ConsoleColor.DarkBlue; //deixar "refrão" em azul
+            Console.WriteLine("Hello again, friend of a friend I knew you when");
+            Console.WriteLine("Our common goal was waiting for the world to end");
+            Console.WriteLine("Now that the truth is just a rule that you can bend");
+            Console.WriteLine("You crack the whip, shape-shift and trick the past again");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.DarkBlue; //deixar "refrão" em azul
             Console.WriteLine("Send you my love on a wire");
-             Console.WriteLine("Lift you up, every time, everyone, ooh, pulls away, ooh");
-              Console.WriteLine("From you");
-              Console.ResetColor();
-               Console.WriteLine();
+            Console.WriteLine("Lift you up, every time, everyone, ooh, pulls away, ooh");
+            Console.WriteLine("From you");
+            Console.ResetColor();
+            Console.WriteLine();
 
             Console.WriteLine("Got balls of steel, got an automobile, for a minimum wage");
-             Console.WriteLine("Got real estate, I'm buying it all up in outer space");
-              Console.WriteLine("Now that the truth is just a rule that you can bend");
-               Console.WriteLine("You crack the whip, shape-shift and trick, the past again");
-                Console.WriteLine();  
+            Console.WriteLine("Got real estate, I'm buying it all up in outer space");
+            Console.WriteLine("Now that the truth is just a rule that you can bend");
+            Console.WriteLine("You crack the whip, shape-shift and trick, the past again");
+            Console.WriteLine();
 
- Console.ForegroundColor = ConsoleColor.DarkBlue; //deixar "refrão" em azul
+            Console.ForegroundColor = ConsoleColor.DarkBlue; //deixar "refrão" em azul
             Console.WriteLine("Send you my love on a wire");
-             Console.WriteLine("Lift you up, every time, everyone, ooh, pulls away, ooh");
-              Console.WriteLine("It's a mechanical bull, the number one");
-               Console.WriteLine("You'll take a ride from anyone");
-                Console.WriteLine("Everyone wants a ride, pulls away, ooh, from you");
-                 Console.ResetColor();
-                  Console.WriteLine();
-            
-
-
-
+            Console.WriteLine("Lift you up, every time, everyone, ooh, pulls away, ooh");
+            Console.WriteLine("It's a mechanical bull, the number one");
+            Console.WriteLine("You'll take a ride from anyone");
+            Console.WriteLine("Everyone wants a ride, pulls away, ooh, from you");
+            Console.ResetColor();
+            Console.WriteLine();
 
         }
     }
